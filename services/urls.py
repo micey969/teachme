9158015f810b5a001b283c69e18bfc28aca5d11e
micey969 <micey_969@hotmail.com>
2022-06-14 14:17:41 -0400

    add about page

--- services/urls.py
+++ services/urls.py
@@ -5,7 +5,9 @@ from django.views.generic import TemplateView
 app_name = 'services'
 
 urlpatterns = [
-    path('termsofuse/',TemplateView.as_view(template_name="services/terms.html"),name='terms'),
-    path('privacy/',TemplateView.as_view(template_name="services/privacy.html"),name='privacy'),
-    path('faqs/',TemplateView.as_view(template_name="services/faqs.html"),name='faqs'),
+    path('termsofuse',TemplateView.as_view(template_name="services/terms.html"),name='terms'),
+    path('privacy',TemplateView.as_view(template_name="services/privacy.html"),name='privacy'),
+    path('faqs',TemplateView.as_view(template_name="services/faqs.html"),name='faqs'),
+    path('about',TemplateView.as_view(template_name="services/about.html"),name='about'),
+
 ]
